cdeda0cd770b9bc0a9f22aa022af66b77f637a26
lysyeah <dldydtn04@naver.com>
2022-09-08 15:31:28 +0900

    123

--- Web study/자바스크립트공부/함수기초.js
+++ Web study/자바스크립트공부/함수기초.js
@@ -93,11 +93,11 @@
 //2.인자
 //3.본문
 //4.리턴값
-function printDan(dan){
-    for(let i=1; i<=9; i++){
-        console.log(`${dan} * ${i} = ${dan*i}`);
-    }
-}
+// function printDan(dan){
+//     for(let i=1; i<=9; i++){
+//         console.log(`${dan} * ${i} = ${dan*i}`);
+//     }
+// }
 // printDan(1);
 
 //문제6.에서 printDan함수내부를 고치지 말고 그대로 활용하여, 구구단 전체를 찍어보세요.
@@ -114,14 +114,71 @@ function printDan(dan){
 // printGugudan()
 
 //문제7. printUpgradeGugudan(n,m)함수    // n단부터 m단까지만 출력해보세요.
-function printUpgradeGugudan(n,m){
+// function printUpgradeGugudan(n,m){
     
-    for (dan=n;dan<=m;dan++){
-        printDan(dan);
-        console.log("");    
+//     for (dan=n;dan<=m;dan++){
+//         printDan(dan);
+//         console.log("");    
+//     }
+    
+// }
+// printUpgradeGugudan(3,5)
+
+
+
+//오후
+
+//문제1. 인자 2 개를 받아서 큰 수에서 작은 수를 뺀 값을 출력하는 함수
+
+//참고한 것.
+// function add(num1,num2,num3){
+//     return num1+num2+num3
+// }
+// console.log(add(3,5,8))
+
+//출력도 하지 않고 있었고, 리턴도 하지 않고 있었다는 말을 듣고 풀었다.
+function MinusSmallFromBig(a,b){
+    if(a > b){
+      return  a-b
+    }else {
+      return  b-a
     }
-    
 }
-printUpgradeGugudan(3,5)
+
+console.log(MinusSmallFromBig(-2,-3));
 
 
+//문제2. 첫 번째 인자 : 문자열, 두 번째 인자 : 숫자
+//      첫 번째 인자를 n번 출력하는 함수.
+            // f("bong",3) bong 을 줄 띄우면서 3 번이 아니라 bongbongbong을 찍어보세요.
+// function printNtimes(inputStr, n){
+//     let result = "";
+//     for(let i =0; i<n; i++){
+//         result+=inputStr;
+//     }
+//     console.log(result);
+// }
+// printNtimes("bong", 3)
+
+//문제3. isOverTen(숫자)는 10보다 크면 true, 이하면 false를 반환하는 함수다.
+        //1번에서 만든 함수의 결과값을 인자로 넘겨 "10보다 큽니다","10 이하입니다"를 출력해보세요.
+function isOverTen(숫자){
+    return 숫자>10? true: false;
+}
+let overTen = isOverTen(diffTwoNum(3,9));
+//방법1
+// (overTen==true)? console.log("10보다 큽니다."): console.log("10이하입니다.")
+//방법2
+if(overTen==true){
+    console.log("10보다 큽니다.");
+}else{
+    console.log("10이하 입니다.");
+}
+
+
+//문제4. calcFigure(도형 이름, 변의 길이)
+    //삼각형, 사각형, 원  3 개의 넓이를 구하는 함수를 만들어보세요.
+    // 가정: 정삼각형, 정사각형, 원은 변의길이를 지름으로 쓴다.
+        //정삼강형공식 = 루트3 / 4 * 한변의 제곱
+        //루트 구하기 = Math.sqrt(숫자)
+    //저 3 개 말고 들어오면 "지원하지 않는 도형입니다."를 출력하기.
